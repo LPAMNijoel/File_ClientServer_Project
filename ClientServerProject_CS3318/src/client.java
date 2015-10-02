11254ddf6a7a6a3210b31c2aebf64dba1a398c8e
LPAMNijoel <nijoeljackson@gmail.com>
2015-10-02 15:59:31 -0500

    added some classes still in debate on the approach GUI wise?

---
+++ ClientServerProject_CS3318/src/client.java
@@ -0,0 +1,16 @@
+import java.*;
+
+
+public class client 
+{
+	private String clientName;
+	private String userName;
+
+	public client(String a, String b)
+	{
+	a = clientName;
+	b = userName;
+
+	}
+}
+
---
+++ ClientServerProject_CS3318/src/groupServer.java
@@ -0,0 +1,17 @@
+import java.*;
+import java.io.*;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class groupServer
+{
+
+	
+	public static void main(String [] args)
+
+	{
+			Scanner keyboard = new Scanner(System.in);
+		
+	}
+}
